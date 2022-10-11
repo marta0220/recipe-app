08d47a0c3c1888d0008aa882425eebb63fd25fb7
marta0220 <109087240+marta0220@users.noreply.github.com>
2022-10-11 21:12:13 +0300

    Created new components

---
+++ src/Ingredients.js
@@ -0,0 +1,5 @@
+import React from "react";
+export default function Ingredients({ ingredients }) {
+  console.log(ingredients);
+  return <div className="Ingredients"></div>;
+}
---
+++ src/Nutrition.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Nutrition({ nutrition }) {
+  return <div className="Nutrition"></div>;
+}
